4d9cdd6709dae4f7e76be006a8a3b6d654663702
Priya Mandal <111956633+PriyaMandall@users.noreply.github.com>
2023-07-21 19:44:21 +0530

    Update Q&A.sql

--- Q&A.sql
+++ Q&A.sql
@@ -1,9 +1,9 @@
 use swiggy;
-# 1. Find Customer who has never ordered
+1. Find Customer who has never ordered
 SELECT u_name FROM users 
 WHERE user_id NOT IN (SELECT user_id FROM orders);
 
-# 2. Average price/dish
+2. Average price/dish
 SELECT f.f_name, avg(price) AS Average_Price
 FROM menu m
 JOIN food f
@@ -11,9 +11,9 @@ ON m.f_id=f.f_id
 GROUP BY f.f_name
 ORDER BY AVG(price) desc;
 
-# 3. Find Top resturant in terms of number of orders for a given month
+3. Find Top resturant in terms of number of orders for a given month
 
-#for the month of june
+June
 SELECT r.r_name, COUNT(*) AS total_order 
 from orders o
 JOIN restaurants r
@@ -22,7 +22,7 @@ WHERE MONTHNAME(date) LIKE 'June'
 GROUP BY r.r_name
 ORDER BY count(*) DESC LIMIT 1;
 
-# for the month of may
+May
 SELECT r.r_name, COUNT(*) AS total_order
 from orders o
 JOIN restaurants r
@@ -31,7 +31,7 @@ WHERE MONTHNAME(date) LIKE 'May'
 GROUP BY r.r_name
 ORDER BY count(*) DESC LIMIT 1;
 
-#for the month of july
+July
 SELECT r.r_name, COUNT(*) AS total_order
 from orders o
 JOIN restaurants r
@@ -40,8 +40,8 @@ WHERE MONTHNAME(date) LIKE 'July'
 GROUP BY r.r_name
 ORDER BY count(*) DESC LIMIT 1;
 
-# 4.Restaurant with monthly sales greate then x
-#june
+4. Restaurant with monthly sales greate then x
+June
 SELECT r.r_name, SUM(amount) AS revenue
 FROM orders o
 JOIN restaurants r
@@ -49,7 +49,7 @@ ON o.r_id=r.r_id
 WHERE monthname(date) LIKE 'June'
 GROUP BY r.r_name HAVING SUM(amount)>500;
 
-#july
+July
 SELECT r.r_name, SUM(amount) AS revenue
 FROM orders o
 JOIN restaurants r
@@ -57,7 +57,7 @@ ON o.r_id=r.r_id
 WHERE monthname(date) LIKE 'July'
 GROUP BY r.r_name HAVING SUM(amount)>500;
 
-#may
+May
 SELECT r.r_name, SUM(amount) AS revenue
 FROM orders o
 JOIN restaurants r
@@ -65,7 +65,7 @@ ON o.r_id=r.r_id
 WHERE monthname(date) LIKE 'May'
 GROUP BY r.r_name HAVING SUM(amount)>500;
 
-# 5.Show all oders with orders details for a perticular customer in a perticular date range
+5. Show all oders with orders details for a perticular customer in a perticular date range
 SELECT o.order_id, r.r_name, od.f_id, f.f_name
 FROM orders o
 JOIN restaurants r
@@ -77,7 +77,7 @@ ON od.f_id=f.f_id
 WHERE user_id = (SELECT user_id FROM users WHERE u_name LIKE 'Khushboo')
 AND date BETWEEN '2023-06-10' AND '2023-07-10' ;
 
-#6. Find restaurants with max repeted orders
+6. Find restaurants with max repeted orders
 SELECT r.r_name, COUNT(*) AS max_customer
 FROM (
 		SELECT r_id, user_id, COUNT(*) AS visits
@@ -90,7 +90,7 @@ ON r.r_id = maxx.r_id
 GROUP BY r.r_name
 ORDER BY max_customer DESC LIMIT 1;
 
-# 6. Month over month revenu growth of swiggy
+ 7. Month over month revenu growth of swiggy
 SELECT month, ((revenue-previous)/previous)*100 AS growth FROM (
 	WITH sales AS 
 	(
@@ -101,7 +101,7 @@ SELECT month, ((revenue-previous)/previous)*100 AS growth FROM (
 SELECT month, revenue, LAG(revenue) OVER (ORDER BY revenue) AS previous FROM sales
 ) growth
 
-# customers name and his/her fav food
+ 8. Customers name and his/her fav food
 WITH temp AS
  (
 	SELECT o.user_id, od.f_id, COUNT(*) AS frequency 
